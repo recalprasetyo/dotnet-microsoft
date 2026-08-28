daa51d5d30aef31b3fa5aa7779eb7ff1f142320c
Recal <iamrecal10@gmail.com>
2026-08-28 23:16:04 +0700

    Get Started with Dotnet

---
+++ 1-get-started-with-dotnet/challenge-1.cs
@@ -0,0 +1,2 @@
+Console.WriteLine("This is the first line");
+Console.Write("This is the second line");
---
+++ 1-get-started-with-dotnet/new-message.cs
@@ -0,0 +1,6 @@
+// Console.Write("Congratulations!");
+// Console.Write(" ");
+// Console.Write("You wrote your first lines of code");
+
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code");
