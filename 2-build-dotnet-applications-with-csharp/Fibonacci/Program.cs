49a0b1927dd2203b69d6cd2357097ecc8de5e4eb
Recal <iamrecal10@gmail.com>
2026-08-31 14:22:51 +0700

    chore: add fibonacci to debug and trace

---
+++ 2-build-dotnet-applications-with-csharp/Fibonacci/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+int result = Fibonacci(5);
+Console.WriteLine(result);
+
+static int Fibonacci(int n)
+{
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
+
+    for (int i = 2; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+        // Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
+
+        // If n2 is 5 continue, else break.
+    }
+    Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
+    return n == 0 ? n1 : n2;
+
+    // return n == 0 ? n1 : n2;
+}
+
+// Console.WriteLine("This message is readable by the end user.");
+// Trace.WriteLine("This is a trace message when tracing the app.");
+// Debug.WriteLine("This is a debug message just for developers.");
+
+// Debug.Write("Debug - ");
+// Debug.WriteLine("This is a full line.");
+// Debug.WriteLine("This is another full line.");
+
+// int count = 0;
+// if (count == 0)
+// {
+//     Debug.WriteLine("The count is 0 and this may cause an exception.");
+// }
+// Debug.WriteLineIf(count == 0, "The count is 0 and this may cause an exception.");
+// bool errorFlag = false;
+// Trace.WriteIf(errorFlag, "Error in AppendData procedure.");
+// Debug.WriteIf(errorFlag, "Transaction abandoned.");
+// Trace.Write("Invalid value for data request");
+
+// int IntegerDivide(int dividend, int divisor)
+// {
+//     Debug.Assert(divisor != 0, $"{nameof(divisor)} is 0 and will cause an exception.");
+
+//     return dividend / divisor;
+// }
