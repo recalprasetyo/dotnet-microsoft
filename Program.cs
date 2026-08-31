621fb68b1590c9b62952bf474c5dc44c4596ed5b
Recal <iamrecal10@gmail.com>
2026-08-31 15:18:10 +0700

    chore: add mslearn-dotnet-files

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+// Directory.CreateDirectory(salesTotalDir);
+// bool doesDirectoryExist = Directory.Exists(salesTotalDir);
+// Console.WriteLine($"Does the directory exist? {doesDirectoryExist}");
+
+var salesFiles = FindFiles(storesDirectory);
+// var salesFiles = FindFiles("stores");
+
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+// File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // var extension = Path.GetExtension(file);
+        // if (extension == ".json")
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    foreach (var file in salesFiles)
+    {
+        string salesJson = File.ReadAllText(file);
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+record SalesData(double Total);
