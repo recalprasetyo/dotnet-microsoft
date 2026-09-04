1497d5cc714cecf7871a8d4a71c2c03042c3897c
Recal <iamrecal10@gmail.com>
2026-09-04 20:28:13 +0700

    feat: add program call methods from dotnet class

---
+++ 4-learn-csharp/part-2/NetClass/Program.cs
@@ -0,0 +1,31 @@
+﻿// call methods
+Random dice = new Random();
+// Random dice = new();
+int roll = dice.Next(1, 7);
+// int result = Random.Next();
+Console.WriteLine(roll);
+
+
+// overloaded methods
+int number = 7;
+string text = "seven";
+
+Console.WriteLine(number);
+Console.WriteLine();
+Console.WriteLine(text);
+
+// Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
