52909f7bf89bae0ef2daf8f5be68058152177dbe
Recal <iamrecal10@gmail.com>
2026-09-09 10:02:20 +0700

    feat: add calculate grades and credit score

---
+++ 4-learn-csharp/part-2/Challenge-1/Program.cs
@@ -0,0 +1,32 @@
+﻿var students = new (string Name, int[] Scores)[]
+{
+    ("Sophia", [95, 98, 96, 92]),
+    ("Andrew", [85, 90, 82, 88]),
+    ("Emma", [92, 94, 89, 95]),
+    ("Logan", [78, 82, 75, 80])
+};
+
+Console.WriteLine($"Student\t\tExam Score\tOverall Grade\tExtra Credit");
+foreach (var student in students)
+{
+    int sum = 0;
+    char grade;
+
+    foreach (var score in student.Scores)
+    {
+        sum += score;
+    }
+
+    decimal examScore = (decimal)sum / student.Scores.Length;
+
+    int scoreInt = (int)examScore;
+
+    decimal creditScore = scoreInt >= 90 ? 5 : (scoreInt >= 80 ? 3 : (scoreInt >= 70 ? 2 : (scoreInt >= 60 ? 1 : 0)));
+
+    grade = examScore >= 90 ? 'A' : (examScore >= 80 ? 'B' : (examScore >= 70 ? 'C' : (examScore >= 60 ? 'D' : 'F')));
+
+    decimal overall = examScore + creditScore;
+
+    Console.WriteLine($"{student.Name}\t\t{examScore}\t\t{overall}\t{grade}\t{scoreInt} ({creditScore})");
+}
+
