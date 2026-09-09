09bd6215f74f2c3f466b66265a56f9fd8a0c33e4
Recal <iamrecal10@gmail.com>
2026-09-09 10:19:31 +0700

    fix: fixing error in overall and credit score

--- 4-learn-csharp/part-2/Challenge-1/Program.cs
+++ 4-learn-csharp/part-2/Challenge-1/Program.cs
@@ -1,32 +1,51 @@
 ﻿var students = new (string Name, int[] Scores)[]
 {
-    ("Sophia", [95, 98, 96, 92]),
-    ("Andrew", [85, 90, 82, 88]),
-    ("Emma", [92, 94, 89, 95]),
-    ("Logan", [78, 82, 75, 80])
+    ("Sophia", [90, 86, 87, 98, 100, 94, 90]),
+    ("Andrew", [92, 89, 81, 96, 90, 89]),
+    ("Emma", [90, 85, 87, 98, 68, 89, 89, 89]),
+    ("Logan", [90, 95, 87, 88, 96, 96 ])
 };
+
+int examAssignments = 5;
 
 Console.WriteLine($"Student\t\tExam Score\tOverall Grade\tExtra Credit");
 foreach (var student in students)
 {
-    int sum = 0;
-    char grade;
+    int gradedAssignments = 0;
+    int gradedExtraCreditAssignments = 0;
 
-    foreach (var score in student.Scores)
+    int sumExamScores = 0;
+    int sumExtraCreditScores = 0;
+
+    decimal currentStudentGrade = 0;
+    decimal currentStudentExamScore = 0;
+    decimal currentStudentExtraCreditScore = 0;
+
+    string grade = "";
+
+    foreach (int score in student.Scores)
     {
-        sum += score;
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+        {
+            sumExamScores = sumExamScores + score;
+        }
+
+        else
+        {
+            gradedExtraCreditAssignments += 1;
+            sumExtraCreditScores += score;
+        }
     }
 
-    decimal examScore = (decimal)sum / student.Scores.Length;
+    currentStudentExamScore = (decimal)(sumExamScores) / examAssignments;
+    currentStudentExtraCreditScore = (decimal)(sumExtraCreditScores) / gradedExtraCreditAssignments;
 
-    int scoreInt = (int)examScore;
+    currentStudentGrade = (decimal)((decimal)sumExamScores + ((decimal)sumExtraCreditScores / 10)) / examAssignments;
 
-    decimal creditScore = scoreInt >= 90 ? 5 : (scoreInt >= 80 ? 3 : (scoreInt >= 70 ? 2 : (scoreInt >= 60 ? 1 : 0)));
+    grade = currentStudentExamScore >= 90 ? "A" : (currentStudentExamScore >= 80 ? "B" : (currentStudentExamScore >= 70 ? "C" : (currentStudentExamScore >= 60 ? "D" : "F")));
 
-    grade = examScore >= 90 ? 'A' : (examScore >= 80 ? 'B' : (examScore >= 70 ? 'C' : (examScore >= 60 ? 'D' : 'F')));
-
-    decimal overall = examScore + creditScore;
-
-    Console.WriteLine($"{student.Name}\t\t{examScore}\t\t{overall}\t{grade}\t{scoreInt} ({creditScore})");
+    Console.WriteLine($"{student.Name}\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{grade}\t{currentStudentExtraCreditScore} ({(((decimal)sumExtraCreditScores / 10) / examAssignments)})");
 }
 
