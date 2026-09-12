1cb3df571b9f70c2b22c4cb0be7f2b3a35a1d2a8
Recal <iamrecal10@gmail.com>
2026-09-12 19:25:28 +0700

    chore: debug cache

--- 4-learn-csharp/part-2/Project-1/obj/Debug/net10.0/Project-1.AssemblyInfo.cs
+++ 4-learn-csharp/part-2/Project-1/obj/Debug/net10.0/Project-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb63f7e74d604d026524ddb509332f1f526dd56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8e0b1bc8572bc848a015a8cfa06f7bd60c26c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
