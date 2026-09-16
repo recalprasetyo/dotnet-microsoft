bb6ab642f5f1bc46964c3827024fbbadf8772c44
Recal <iamrecal10@gmail.com>
2026-09-16 14:41:08 +0700

    chore: add initial project files including .gitignore and Program.cs

---
+++ 4-learn-csharp/part-3/CodeBlock/Program.cs
@@ -0,0 +1,22 @@
+﻿// Code sample 1
+// bool flag = true;
+// int value;
+
+// if (flag)
+// {
+//     value = 10;
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
+
+// Console.WriteLine($"Outside the code block: {value}");
+
+// Code sample 2
+int value;
+
+if (true)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
