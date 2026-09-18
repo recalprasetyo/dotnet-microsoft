de16033b31061f04de893a72f7cb6b12c68ef408
Recal <iamrecal10@gmail.com>
2026-09-18 17:42:28 +0700

    chore: comment out code samples in Program.cs for clarity

--- 4-learn-csharp/part-3/CodeBlock/Program.cs
+++ 4-learn-csharp/part-3/CodeBlock/Program.cs
@@ -11,28 +11,43 @@
 // Console.WriteLine($"Outside the code block: {value}");
 
 // Code sample 2
-int value;
+// int value;
 
-if (true)
-{
-    value = 10;
-    Console.WriteLine($"Inside the code block: {value}");
-}
+// if (true)
+// {
+//     value = 10;
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
 
-Console.WriteLine($"Outside the code block: {value}");
+// Console.WriteLine($"Outside the code block: {value}");
 
-string name = "steve";
-if (name == "bob") Console.WriteLine("Found Bob");
-else if (name == "steve") Console.WriteLine("Found Steve");
-else Console.WriteLine("Found Chuck");
+// string name = "steve";
+// if (name == "bob") Console.WriteLine("Found Bob");
+// else if (name == "steve") Console.WriteLine("Found Steve");
+// else Console.WriteLine("Found Chuck");
 
 // compare code
 
-string name2 = "steve";
+// string name2 = "steve";
 
-if (name2 == "bob")
-    Console.WriteLine("Found Bob");
-else if (name2 == "steve")
-    Console.WriteLine("Found Steve");
-else
-    Console.WriteLine("Found Chuck");
+// if (name2 == "bob")
+//     Console.WriteLine("Found Bob");
+// else if (name2 == "steve")
+//     Console.WriteLine("Found Steve");
+// else
+//     Console.WriteLine("Found Chuck");
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+foreach (int number in numbers)
+{
+    int total = 0;
+
+    total += number;
+
+    if (number == 42) bool found = true;
+}
+
+if (found) Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
