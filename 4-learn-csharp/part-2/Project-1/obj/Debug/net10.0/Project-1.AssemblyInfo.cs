828b2c10b33e699b29576b43dfd8e4405e019dfa
Recal <iamrecal10@gmail.com>
2026-09-23 15:29:05 +0700

    chore: update assembly informational version and cache inputs

--- 4-learn-csharp/part-2/Project-1/obj/Debug/net10.0/Project-1.AssemblyInfo.cs
+++ 4-learn-csharp/part-2/Project-1/obj/Debug/net10.0/Project-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78429928ec41ae8d3329252b8ca7c39ccc72527d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c724cb5351f6a9705d663802ee16301d1f7fe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
